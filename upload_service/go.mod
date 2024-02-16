4afa39becce41cfcc7978de7f0e8a534fdc38bdd
Julian <liannsd3@gmail.com>
2024-02-15 21:58:33 -0300

    upload_service

---
+++ upload_service/go.mod
@@ -0,0 +1,56 @@
+module upload
+
+go 1.21.3
+
+require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20230828082145-3c4c8a2d2371 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.25.0 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.27.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.15.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.17.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.49.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.19.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.22.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.27.0 // indirect
+	github.com/aws/smithy-go v1.20.0 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
+	github.com/go-git/go-git/v5 v5.11.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/labstack/echo/v4 v4.11.4 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/skeema/knownhosts v1.2.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+)
---
+++ upload_service/main.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"context"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/joho/godotenv"
+	"log"
+
+	"github.com/go-redis/redis"
+
+	"log/slog"
+)
+
+type App struct {
+	Client *s3.Client
+	Db     *redis.Client
+}
+
+func main() {
+
+	slog.Info("Starting gitbit")
+	redisdb := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+
+	slog.Info("Redis client created")
+	redisdb.WrapProcess(func(old func(cmd redis.Cmder) error) func(cmd redis.Cmder) error {
+		return func(cmd redis.Cmder) error {
+			fmt.Printf("starting processing: <%s>\n", cmd)
+			err := old(cmd)
+			fmt.Printf("finished processing: <%s>\n", cmd)
+			return err
+		}
+	})
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bucket := os.Getenv("S3_BUCKET")
+
+	fmt.Println("Hello, World!")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("sa-east-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+	})
+
+	for _, object := range output.Contents {
+		fmt.Println(aws.ToString(object.Key))
+	}
+
+	if err != nil {
+		slog.Error("unable to list buckets, %v", err)
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	app := &App{Client: client, Db: redisdb}
+
+	e.POST("/deploy", app.deploy)
+
+	e.GET("/status/:id", func(c echo.Context) error {
+
+		id := c.Param("id")
+		res, err := app.Db.HGet(id, "status").Result()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, "Error getting status")
+		}
+		return c.JSON(http.StatusOK, res)
+
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type Repo struct {
+	URL string `json:"repo"`
+}
+
+func (a *App) deploy(c echo.Context) error {
+	repo := new(Repo)
+	if err := c.Bind(repo); err != nil {
+		return err
+	}
+
+	ch := make(chan string)
+
+	var wg sync.WaitGroup
+
+	id, err := generateId()
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Error generating id")
+	}
+
+	slog.Info("Cloning repo")
+	err = clone(repo.URL, id)
+	slog.Info("Cloning repo" + repo.URL)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Error cloning repo")
+	}
+
+	files, err := getFiles(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Error getting files")
+	}
+
+	for _, file := range files {
+		wg.Add(1)
+		go uploadFile(file, "gitbit", a.Client, ch, &wg)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, "Error uploading file")
+		}
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	res := struct {
+		Id string `json:"id"`
+	}{Id: id}
+
+	//Enqueue the id of the repo to be built in the other service
+	a.Db.LPush("build-queue", id)
+	//Set the status of the repo to uploaded
+	a.Db.HSet(id, "status", "uploaded")
+
+	slog.Info("Deployed repo " + repo.URL + " with id " + id)
+	return c.JSON(http.StatusOK, res)
+}
+
+func generateId() (string, error) {
+
+	id, err := uuid.NewRandom()
+	//grab the first 5 digits of the uuid
+	//and return it as a string
+
+	return id.String()[:5], err
+}
+
+func clone(repoURL string, id string) error {
+
+	_, err := git.PlainClone("./out/"+id, false, &git.CloneOptions{
+		URL:      repoURL,
+		Progress: os.Stdout,
+	})
+
+	return err
+}
+
+func getFiles(id string) ([]string, error) {
+	files := []string{}
+
+	err := filepath.Walk("out/"+id, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// skip directories
+		if info.IsDir() {
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	})
+
+	return files, err
+}
+
+func uploadFile(fileName string, bucketName string, client *s3.Client, ch chan<- string, wg *sync.WaitGroup) error {
+	fileContent, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file")
+		return err
+	}
+
+	defer wg.Done()
+	defer fileContent.Close()
+
+	result, err := client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileName),
+		Body:   fileContent,
+	})
+
+	if err != nil {
+		slog.Error("failed to upload file %s to bucket %s: %w", fileName, "gitbit", err)
+		return err
+
+	}
+	fmt.Println(result)
+
+	ch <- fileName
+	return nil
+}
